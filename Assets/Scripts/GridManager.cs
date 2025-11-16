3cc782296478576ae72aba1599f7cd9764655f2a
auchija <agonzalezr747@gmail.com>
2025-11-16 12:16:07 -0500

    avance gatos

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -25,7 +25,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(transform.position.x + x, transform.position.y + y), Quaternion.identity);
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(transform.position.x + x, transform.position.y + y -0.5f), Quaternion.identity);
                 spawnedTile.name = $"Tile {x}, {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
